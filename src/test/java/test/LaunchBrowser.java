9ad390597c3b92af0bad982b35ca5777caef1805
nganlam100 <nganlam100@gmail.com>
2022-04-18 22:48:55 +0700

    start browser

--- src/test/java/test/LaunchBrowser.java
+++ src/test/java/test/LaunchBrowser.java
@@ -1,4 +1,23 @@
 package test;
 
+import org.checkerframework.checker.units.qual.C;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+
 public class LaunchBrowser {
+    public static void main(String[] args) throws InterruptedException {
+        String currentProjectLocation = System.getProperty("user.dir");
+        String relativeChromeDriverPath = "/src/test/resources/drivers/chromedriver.exe";
+        String chromeDriverPath = currentProjectLocation.concat(relativeChromeDriverPath);
+        System.setProperty("webdriver.chrome.driver",chromeDriverPath);
+        ChromeOptions chromeOptions = new ChromeOptions();
+        chromeOptions.getBrowserName();
+        WebDriver driver = new ChromeDriver();
+        driver.get("https://www.selenium.dev/downloads/");
+        driver.manage().window().maximize();
+
+        Thread.sleep(1000);
+        driver.quit();
+    }
 }
